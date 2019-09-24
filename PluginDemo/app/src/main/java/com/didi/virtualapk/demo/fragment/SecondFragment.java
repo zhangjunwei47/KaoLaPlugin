df8326dbf94a8cc8e3f204f8b92bc55a9b756519
zhangchao <zhangchao1@kaolafm.com>
2019-09-24 14:58:25 +0800

    验证成功

--- PluginDemo/app/src/main/java/com/didi/virtualapk/demo/fragment/SecondFragment.java
+++ PluginDemo/app/src/main/java/com/didi/virtualapk/demo/fragment/SecondFragment.java
@@ -6,17 +6,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.didi.virtualapk.demo.R;
 
 import me.yokeyword.fragmentation.SupportFragment;
 
-public class LauncherFragment extends SupportFragment {
-    ImageView imageView;
+public class SecondFragment extends SupportFragment {
 
-    public static LauncherFragment getInstance() {
-        LauncherFragment launcherFragment = new LauncherFragment();
+    public static SecondFragment getInstance() {
+        SecondFragment launcherFragment = new SecondFragment();
         return launcherFragment;
     }
 
@@ -28,14 +26,7 @@ public class LauncherFragment extends SupportFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_launcher, container, false);
-        imageView = view.findViewById(R.id.iv1);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                extraTransaction().start(new SecondFragment());
-            }
-        });
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
         return view;
     }
 
