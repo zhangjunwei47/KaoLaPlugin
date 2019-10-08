50b9c8492e7c9f04708ee805c26c18b799614a5b
zhangchao <zhangchao1@kaolafm.com>
2019-10-08 20:10:06 +0800

    test

--- PluginDemo/make.sh
+++ PluginDemo/make.sh
@@ -1,4 +1,5 @@
 ./gradlew clean assemblePlugin
 adb push app/build/outputs/apk/beijing/release/app-beijing-release.apk /sdcard/Test.apk
 adb shell am force-stop com.didi.virtualapk
+
 adb shell am start -n com.didi.virtualapk/com.didi.virtualapk.MainActivity
